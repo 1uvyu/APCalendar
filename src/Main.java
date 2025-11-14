455ef4cba851f41658ffa3c7a9628d3e0475199a
KayleeChan <kayleec55@nycstudents.net>
2025-11-14 12:15:30 -0500

    Added test cases for numberofLeapYears and dayOfWeek

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(APCalendar.numberOfLeapYears(2000, 2025));
+        System.out.println(APCalendar.numberOfLeapYears(1954, 2025));
+        System.out.println(APCalendar.dayOfWeek(1, 5, 2019));
+        System.out.println(APCalendar.dayOfWeek(1, 10, 2019));
+    }
 }
